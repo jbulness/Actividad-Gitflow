35b863165c75936c83f336ea43cd1468b93517dd
jbulness <48481857+jbulness@users.noreply.github.com>
2022-03-28 19:25:26 -0300

    feat(main): new main function and Readme changes

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Actividad-Gitflow
+
+Para ejecutar el código se debe descargar la base de datos indicada en el enunciado de la actividad y colocarlo dentro del directorio del repositorio local.
--- top10Hashtags.py
+++ top10Hashtags.py
@@ -1,3 +1,3 @@
 def top_10_hashtags(df):
     # Función basada en el código obtenido del siguiente link: https://stackoverflow.com/questions/63228403/count-hashtag-frequency-in-a-dataframe 
-    return df['content'].str.findall(r'(#\w+)').explode().value_counts()
+    return df['content'].str.findall(r'(#\w+)').explode().value_counts().head(10)
